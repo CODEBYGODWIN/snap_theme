ca978f5ddcc75cf2c95deca56b50156a2994f018
Cy4n1 <156065290+Cassian-J@users.noreply.github.com>
2026-07-06 08:01:27 +0200

    add front for the lobby te create room and the join room

---
+++ lib/screens/lobby_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class LobbyScreen extends StatelessWidget {
+  final String roomId;
+  final String userId;
+
+  const LobbyScreen({
+    super.key,
+    required this.roomId,
+    required this.userId,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final roomRef = FirebaseFirestore.instance.collection("rooms").doc(roomId);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Lobby")),
+      body: Column(
+        children: [
+
+          // 👥 PLAYERS LIVE
+          StreamBuilder(
+            stream: roomRef.collection("players").snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const CircularProgressIndicator();
+
+              final players = snapshot.data!.docs;
+
+              return Expanded(
+                child: ListView(
+                  children: players.map((p) {
+                    return ListTile(
+                      title: Text(p["displayName"]),
+                      subtitle: Text("Score: ${p["score"]}"),
+                    );
+                  }).toList(),
+                ),
+              );
+            },
+          ),
+          StreamBuilder(
+            stream: roomRef.snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const SizedBox();
+
+              final data = snapshot.data!;
+              final hostId = data["hostId"];
+
+              return StreamBuilder(
+                stream: roomRef.collection("players").snapshots(),
+                builder: (context, snap) {
+                  if (!snap.hasData) return const SizedBox();
+
+                  final players = snap.data!.docs;
+
+                  final canStart =
+                      hostId == userId && players.length >= 3;
+
+                  return ElevatedButton(
+                    onPressed: canStart
+                        ? () async {
+                            await roomRef.update({
+                              "status": "playing",
+                            });
+                          }
+                        : null,
+                    child: const Text("Démarrer la partie"),
+                  );
+                },
+              );
+            },
+          ),
+
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
