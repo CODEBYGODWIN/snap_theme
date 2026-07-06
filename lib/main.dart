3163539f40bbf58d0fadb1a0cb66fe4891b4edc1
Emmanuel YOHORE <emmanuelbasket01@gmail.com>
2026-07-06 09:29:48 +0200

    persistence data

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'firebase_options.dart'; 
-import 'screens/auth/auth_gate.dart';
+import 'screens/auth/home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (kIsWeb) {
+    await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+  }
   runApp(const ProviderScope(child: SnapThemeApp()));
 }
 
@@ -21,7 +27,7 @@ class SnapThemeApp extends StatelessWidget {
     return MaterialApp(
       title: 'SnapThème',
       debugShowCheckedModeBanner: false,
-      home: const AuthGate(),
+      home: const Home(),
     );
   }
 }
--- lib/screens/auth/home.dart
+++ lib/screens/auth/home.dart
@@ -4,8 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../providers/auth_providers.dart';
 import 'login_screen.dart';
 
-class AuthGate extends ConsumerWidget {
-  const AuthGate({super.key});
+class Home extends ConsumerWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
